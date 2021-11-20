ff989d9dbbd79a2bb8b5d57c6ef9fa42a3c285f4
Erin Hancock <erinhancock794@gmail.com>
2021-11-20 16:53:44 -0700

    set up html and css for main page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 console.log('public script running');
 
-fetch('.netlify/functions/helloWorld')
-.then(res => res.json())
-.then(data => console.log('data-->', data));
+// fetch('.netlify/functions/helloWorld')
+// .then(res => res.json())
+// .then(data => console.log('data-->', data));
