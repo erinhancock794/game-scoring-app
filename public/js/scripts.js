88a9b58ab6b1628411505a4cbef8c4a201c662ce
Erin Hancock <erinhancock794@gmail.com>
2021-11-20 18:29:40 -0700

    started linking input data with javascript, added a new netlify function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,47 @@
-console.log('public script running');
+(function(window) {
 
-// fetch('.netlify/functions/helloWorld')
-// .then(res => res.json())
-// .then(data => console.log('data-->', data));
+
+    console.log('public script running');
+    const newGameForm = document.querySelector("#new-game-form");
+
+    const createNewGameDiv = document.querySelector('#new-game-div');
+    console.log('createNewGameDiv------>', createNewGameDiv);
+    const newGameNameInput = document.querySelector('#game-name-input');
+    console.log('newGameNameInput---->', newGameNameInput);
+
+    const maxScoreInput = document.querySelector("#maxScore");
+    const highScoreToggle = document.querySelector("#highScoreWins");;
+
+
+
+    const startGameBtn = document.querySelector("#start-game-btn");
+
+    startGameBtn.addEventListener('click', () => {
+        submitNewGameInput(newGameForm);
+    })
+
+    function submitNewGameInput(newGameForm) {
+        console.log('newGameForm----', newGameForm)
+
+    }
+
+    fetch('.netlify/functions/helloWorld')
+        .then(res => res.json())
+        .then(data => console.log('data-->', data));
+
+
+    fetch('.netlify/functions/newGame', {
+            method: "PUT",
+            body: JSON.stringify({ body: 'newGame endpoint data' }),
+            headers: getHeaders()
+        })
+        .then(res => res.json())
+        .then(data => console.log('newGame data-->', data));
+
+
+    function getHeaders() {
+        return {
+            "Content-Type": "application/json; charset=UTF-8",
+        };
+    }
+})(window);
